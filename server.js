86cdff08c343faa21b93e8c91fd03c341684e47a
AKA <acuteangel@users.noreply.github.com>
2018-12-05 15:29:28 -0800

    added dynamic port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var apiroutes = require('./app/routing/apiRoutes');
 var htmlroutes = require('./app/routing/htmlRoutes');
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
